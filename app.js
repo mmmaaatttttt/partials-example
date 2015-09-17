29bd38e89be9addce3a4d027d9ab7487e61d06f7
Matt Lane <lane.matthew@gmail.com>
2015-09-17 13:50:50 -0700

    index

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var express = require('express'), 
 app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.listen(3000, function() {
+  console.log("go to localhost:3000 for some sweet demos!");
+});
