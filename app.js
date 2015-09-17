789ca8e18dfb6e99c43702e1e3a3ad600d6ba204
Matt Lane <lane.matthew@gmail.com>
2015-09-17 14:23:26 -0700

    about page, some styling

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ var express = require('express'),
 app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+});
+
+app.get('/contact', function (req, res) {
+  res.render('contact');
 });
 
 app.listen(3000, function() {
